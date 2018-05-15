304ae157ad8e10ea926180143cdc4c563453e22f
Park9eon <quiet9.noise@gmail.com>
2018-05-15 18:01:26 +0900

    기본 컨텐츠 controller 수정 중

--- src/main/kotlin/com/park9eon/home/controller/ContentController.kt
+++ src/main/kotlin/com/park9eon/home/controller/ContentController.kt
@@ -2,8 +2,10 @@ package com.park9eon.home.controller
 
 import com.park9eon.home.annotation.Me
 import com.park9eon.home.command.ContentCommand
+import com.park9eon.home.domain.Content
 import com.park9eon.home.domain.User
 import com.park9eon.home.service.ContentService
+import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,23 +14,30 @@ open class ContentController(
         val contentService: ContentService
 ) {
 
-//    @GetMapping
-//    fun index(@RequestParam(name = "offset", required = false, defaultValue = "0") page: Int) =
-//            this.contentService.getAll(page)
-//
-//    @GetMapping("/{id}")
-//    fun show(@PathVariable(name = "id") id: Long) =
-//            this.contentService.getOne(id)
-//
-//    @PostMapping
-//    fun save(@Me me: User, @RequestBody contentCommand: ContentCommand) =
-//            this.contentService.save(me.id, contentCommand.source, contentCommand.type)
-//
-//    @PutMapping("/{id}")
-//    fun update(@Me me: User, @PathVariable(name = "id") id: Long, @RequestBody contentCommand: ContentCommand) =
-//            this.contentService.update(me.id, id, contentCommand.source)
-//
-//    @DeleteMapping("/{id}")
-//    fun delete(@Me me: User, @PathVariable(name = "id") id: Long) =
-//            this.contentService.delete(me.id, id)
+    @GetMapping
+    fun index(@Me me: User? = null,
+              @RequestParam(name = "c", required = false) categoryId: Long? = null,
+              @RequestParam(name = "p", required = false, defaultValue = "0") offset: Int) =
+            this.contentService.getContents(categoryId = categoryId, offset = offset)
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable(name = "id") id: Long) =
+            this.contentService.getContent(id)
+
+    @PostMapping
+    @Secured("ROLE_USER")
+    fun save(@Me me: User, @RequestBody content: Content) =
+            this.contentService.saveContent(content)
+
+    @PutMapping("/")
+    @Secured("ROLE_USER")
+    fun update(@Me me: User, @PathVariable(name = "id") id: Long, @RequestBody content: Content) =
+            this.contentService.updateContent(content)
+
+    @DeleteMapping("/{id}")
+    @Secured("ROLE_USER")
+    fun delete(@Me me: User, @PathVariable(name = "id") id: Long) =
+            this.contentService.deleteContent(me.id)
+
+    // @PutMapping("/{id}/tag/{tagName}")
 }
